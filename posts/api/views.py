caad70813f8716451db07cb5a892e94bc79dfbc3
Alex <dnjscksdn98@gmail.com>
2020-03-07 20:27:46 +0900

    handle exceptions in CommentView

--- posts/api/views.py
+++ posts/api/views.py
@@ -2,7 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+from rest_framework.status import (
+    HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_401_UNAUTHORIZED
+)
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 
@@ -26,24 +28,31 @@ class PostView(RetrieveAPIView):
 class CommentView(APIView):
     def post(self, request, *args, **kwargs):
         comment = request.data.get('comment', None)
-        post_id = request.data.get('blogId')
+        post_id = request.data.get('blogId', None)
 
         if comment is None:
-            return Response({'message': 'Invalid comment received.'}, status=HTTP_400_BAD_REQUEST)
+            return Response({'message': 'Invalid comment received.'}, status=HTTP_404_NOT_FOUND)
+
+        if post_id is None:
+            return Response({'message': 'This blog does not exist.'}, status=HTTP_404_NOT_FOUND)
 
         try:
             post = Post.objects.get(id=post_id)
         except ObjectDoesNotExist:
-            raise Http404('This page does not exist.')
+            raise Http404('This blog does not exist.')
 
-        new_comment = Comment(
-            user=request.user,
-            content=comment,
-            post=post
-        )
-        new_comment.save()
+        if request.user.is_authenticated:
+            new_comment = Comment(
+                user=request.user,
+                content=comment,
+                post=post
+            )
+            new_comment.save()
 
-        post.comments.add(new_comment)
-        post.save()
+            post.comments.add(new_comment)
+            post.save()
 
-        return Response({'message': 'Successfully submitted a comment.'}, status=HTTP_200_OK)
+            return Response({'message': 'Successfully submitted a comment.'}, status=HTTP_201_CREATED)
+
+        else:
+            return Response({'message': 'Please login first.'}, status=HTTP_401_UNAUTHORIZED)
