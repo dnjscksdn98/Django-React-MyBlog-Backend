ffa361f33794aff37ff496a31f68c1f855fdb122
Alex <dnjscksdn98@gmail.com>
2020-03-18 20:29:29 +0900

    update files

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -6,14 +6,9 @@ from .views import (
 
 urlpatterns = [
     path('posts/', PostsView.as_view(), name='posts'),
-    # path('posts/', PostCreateView.as_view(), name='post-create'),
     path('posts/<pk>/', PostDetailView.as_view(), name='post-detail'),
-    # path('posts/<pk>/', PostUpdateView.as_view(), name='post-update'),
-    # path('posts/<pk>/', PostDeleteView.as_view(), name='post-delete'),
     path('posts/<pk>/comments/', CommentView.as_view(), name='create-comment'),
-
     path('categories/', CategoryView.as_view(), name='get-categories'),
-
     path('users/id/', UserIdView.as_view(), name='get-user-id'),
     path('users/<pk>/profile/', UserProfileView.as_view(), name='user-profile'),
 ]
